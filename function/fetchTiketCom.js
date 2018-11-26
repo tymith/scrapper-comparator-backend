bd7854ee5d270c7f0757e694dab9cfb2b033d129
titusefferian <kiritosato@gmail.com>
2018-11-26 22:39:37 +0700

    add scrap for tiket and airy and traveloka, need refactor soon

---
+++ function/fetchTiketCom.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const fetchTiketCom = (data) => {
+    let url = 'https://www.tiket.com/ms-gateway/tix-flight-search/search?origin=JKTC&destination=DPS&departureDate=2018-11-29&adult=1&child=0&infant=0&cabinClass=ECONOMY&originType=CITY&destinationType=AIRPORT&searchType=ONE_WAY&resultType=ALL'   
+}
+
+export default fetchTiketCom
